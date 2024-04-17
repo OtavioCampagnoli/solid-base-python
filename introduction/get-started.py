9f6933701650d4d92ca39d1942670fab746e6eb5
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-04-16 23:42:27 -0300

    Python Get Started: Interpreted my first file.py

---
+++ introduction/get-started.py
@@ -0,0 +1,15 @@
+print("My name is Otavio Campagnoli, thanks to interpret me!")
+# To check if you have python installed on a linux PC, use the follow command on terminal:
+
+# python --version
+
+# Python is an interpreted programming language, this means that as a developer you write Python (.py) files in a text editor and then put those files into the python interpreter to be executed.
+
+# example:
+
+# python nameFile.py
+
+## Simple as that. Save your file. Open your command line, navigate to the directory where you saved your file, and run:
+
+# cd ./introduction
+# python get-started.py
