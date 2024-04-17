24cb14ef3772411ed71a80c5970be9c3a52697e7
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-04-17 17:59:08 -0300

    Variables Python Concept: Many Values to Multiple.

---
+++ introduction/assignMultipleValues.py
@@ -0,0 +1,10 @@
+# Python Variables - Assign Multiple Values
+# Many Values to Multiple Variables
+# Python allows you to assign values to multiple variables in one line:
+
+x, y, z = "Banana", "Apple", "Orange"
+print(x)
+print(y)
+print(z)
+
+# Note: Make sure the number of variables matches the number of values, or else you will get an error.
