db045ca6506d655dbefa0062e5539587eb3e423f
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-04-16 22:31:29 -0300

    Home Python: Apresentantion of language.

---
+++ introduction/home.py
@@ -0,0 +1,48 @@
+# Python is a popular programming language.
+
+# Python can be used on a server to create web applications.
+
+print("Hello, World!")
+
+# Python File Handling
+# In our File Handling section you will learn how to open, read, write, and delete files.
+
+# Python Database Handling
+# In our database section you will learn how to access and work with MySQL and MongoDB databases:
+
+
+# Python Reference
+# You will also find complete function and method references:
+
+# Reference Overview
+
+# Built-in Functions
+
+# String Methods
+
+# List/Array Methods
+
+# Dictionary Methods
+
+# Tuple Methods
+
+# Set Methods
+
+# File Methods
+
+# Python Keywords
+
+# Python Exceptions
+
+# Python Glossary
+
+# Random Module
+
+# Requests Module
+
+# Math Module
+
+# CMath Module
+
+# Download Python
+# Download Python from the official Python web site: https://python.org
