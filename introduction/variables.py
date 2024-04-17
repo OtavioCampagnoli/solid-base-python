28f690a7fd89f585dcc9ca512cf6407878649d09
OtavioCampagnoli <otaviocampagnoli@hotmail.com>
2024-04-17 13:34:17 -0300

    Variables Python Concept: Creating variables.

---
+++ introduction/variables.py
@@ -0,0 +1,21 @@
+"""Python Variables
+    Variables
+    Variables are containers for storing data values.
+
+    Creating Variables
+    Python has no command for declaring a variable.
+
+    A variable is created the moment you first assign a value to it.
+"""
+x = 5
+y = "Otavio"
+print(x)
+print(y)
+
+# info: Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+x = 4       # x is of type int
+x = "Campagnoli" # x is now of type str
+print(x)
+y = 10
+print(y)
